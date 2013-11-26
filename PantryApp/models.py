41e9d9e3fa397e953425b41590767ab37f3b740e
Sean Kelley <smskelley@gmail.com>
2013-11-25 20:09:07 -0500

    Add __unicode__ methods to models.

--- PantryApp/models.py
+++ PantryApp/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Ingredient Model
 class Ingredient(models.Model):
         name = models.CharField(max_length=200)
+        def __unicode__(self):
+            return self.name
 
 # Category Model
 class Category(models.Model):
         name = models.CharField(max_length=200)
+        def __unicode__(self):
+            return self.name
 
 # User Model
 class User(models.Model):
@@ -14,6 +18,8 @@ class User(models.Model):
 	password_hash = models.CharField(max_length=200)
 	session_id = models.IntegerField()
 	ingredients = models.ManyToManyField(Ingredient)
+        def __unicode__(self):
+            return self.email
 
 # Recipe Model
 class Recipe(models.Model):
@@ -22,3 +28,5 @@ class Recipe(models.Model):
 	photo_exists = models.BooleanField()
 	category = models.ForeignKey(Category)
 	ingredients = models.ManyToManyField(Ingredient)
+        def __unicode__(self):
+            return self.name
