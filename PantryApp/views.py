8ecf0176a5d188c928f8f9dc8e3a5d8870bbb914
Sean Kelley <smskelley@gmail.com>
2013-11-30 23:41:02 -0500

    Add view stubs to test templates

--- PantryApp/views.py
+++ PantryApp/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic.base import View
 
 # Register View
 class Register(View):
-	
 	def get(self, request):
-		return HttpResponse("Hello, World!")
+		return render(request, 'PantryApp/register.html')
 
+class Login(View):
+	def get(self, request):
+		return render(request, 'PantryApp/login.html')
+
+class Pantry(View):
+	def get(self, request):
+		return render(request, 'PantryApp/pantry.html')
+
+class Recipes(View):
+	def get(self, request):
+		return render(request, 'PantryApp/recipes.html')
