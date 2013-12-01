f35efb737d5c80294d97558bd11ddf7cc17ea1fa
Sean Kelley <smskelley@gmail.com>
2013-11-30 23:38:37 -0500

    Add basic templates. None utilize passed in data yet.

---
+++ PantryApp/static/PantryApp/js/fnp_forms.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    var minPassLen = 5;
+    var invalidPasswords = function(pass1, pass2) {
+        if (pass1 != pass2) {
+            return "Passwords don't match";
+        }
+        if (pass1.length < minPassLen) {
+            return "Password is too short";
+        }
+        return false;
+    };
+    var validEmail = function(email) {
+        if (!email) {
+            return false;
+        }
+        try {
+            email.split("@")[1].split(".").length;
+        } catch (err) {
+            return false;
+        }
+        return true;
+    };
+    var addFormErrorMsg = function(msg,loc) {
+        $("<p class=\"form_error\">" + msg + "</p>").insertAfter(loc);
+    };
+
+    $('#login').click(function(event){
+        event.preventDefault();
+        // clean the slate each time
+        $('.form_error').remove();
+        
+        if ($('#pass').val().length < minPassLen) {
+            // notify user the passwords don't match
+            addFormErrorMsg("Password is too short", '#pass');
+        }
+        if (!validEmail($('#email').val())) {
+            addFormErrorMsg("Invalid email address", '#email');
+        }
+        if (!$('.form_error').length) {
+            // if we've gotten this far and there's no error messages, the form
+            // is likely valid, so post it.
+            console.log("valid form.");
+        }
+    });
+
+    $('#register').click(function(event){
+        event.preventDefault();
+        // clean the slate each time
+        $('.form_error').remove();
+        
+        var error;
+        if (error = invalidPasswords($('#pass1').val(), $('#pass2').val())) {
+            // notify user the passwords don't match
+            addFormErrorMsg(error, '#pass2');
+        }
+        if (!validEmail($('#email').val())) {
+            addFormErrorMsg("Invalid email address", '#email');
+        }
+        if (!$('.form_error').length) {
+            // if we've gotten this far and there's no error messages, the form
+            // is likely valid, so post it.
+            console.log("valid form.");
+        }
+    });
+});
