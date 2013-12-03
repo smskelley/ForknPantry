897adeaa36216c7bb534c19ae43a048c0c4089c9
Sean Kelley <smskelley@gmail.com>
2013-12-02 20:47:40 -0500

    Provide test dictionaries to render templates with.

--- PantryApp/views.py
+++ PantryApp/views.py
@@ -4,17 +4,46 @@ from django.views.generic.base import View
 
 # Register View
 class Register(View):
-	def get(self, request):
-		return render(request, 'PantryApp/register.html')
+    def get(self, request):
+        return render(request, 'PantryApp/register.html')
+    def post(self, request):
+        # Rendering example data
+        return render(request, 'PantryApp/register.html', { 'email': 'user@mail.com' })
 
 class Login(View):
-	def get(self, request):
-		return render(request, 'PantryApp/login.html')
+    def get(self, request):
+        return render(request, 'PantryApp/login.html')
+    def post(self, request):
+        # Rendering example data
+        return render(request, 'PantryApp/login.html', { 'user': 'user@mail.com' })
 
 class Pantry(View):
-	def get(self, request):
-		return render(request, 'PantryApp/pantry.html')
+    def get(self, request):
+        # Rendering example data
+        return render(request, 'PantryApp/pantry.html',
+                {"ingredients": [
+                    {'id': 1, 'ingredient': 'Milk', 'user_has': True },
+                    {'id': 2, 'ingredient': 'Eggs', 'user_has': False },
+                    {'id': 3, 'ingredient': 'Cheese', 'user_has': True },
+                ]})
+    def post(self, request):
+        # Rendering example data
+        return render(request, 'PantryApp/pantry.html',
+                {"ingredients": [
+                    {'id': 1, 'ingredient': 'Milk', 'user_has': False },
+                    {'id': 2, 'ingredient': 'Eggs', 'user_has': True },
+                    {'id': 3, 'ingredient': 'Cheese', 'user_has': False },
+                ]})
 
 class Recipes(View):
-	def get(self, request):
-		return render(request, 'PantryApp/recipes.html')
+    def get(self, request):
+        # Rendering example data
+        return render(request, 'PantryApp/recipes.html',
+                {"recipes": [
+                    {'id': 1, 'name': 'Chili', 'link': 'www.food.com', 
+                        'photo_exists': False },
+                    {'id': 2, 'name': 'Chili', 'link': 'www.food.com', 
+                        'photo_exists': False },
+                    {'id': 3, 'name': 'Chili', 'link': 'www.food.com', 
+                        'photo_exists': False },
+                ]})
